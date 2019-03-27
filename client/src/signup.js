3e6810aef790fec5eb47bb1850bcf3c683be7946
japhex <j@japhex.net>
2019-03-27 08:09:35 +0000

    Refactor of signup form

--- client/src/signup.js
+++ client/src/signup.js
@@ -1,49 +1,37 @@
-import React, {Component, Fragment} from "react";
-import AuthHelperMethods from './components/AuthHelperMethods';
-import axios from "axios";
+import React, {useState} from "react"
+import AuthAPI from 'api/auth'
 import { Link } from 'react-router-dom';
 
-export default class Signup extends Component {
-    Auth = new AuthHelperMethods();
+const Signup = ({history}) => {
+	const [username, setUsername] = useState("");
+	const [password, setPassword] = useState("");
 
-    state = {
-        username: "",
-        password: ""
+    const handleChange = e => {
+    	if (e.target.name === 'username') {
+		    setUsername(e.target.value)
+	    }
+	    if (e.target.name === 'password') {
+		    setPassword(e.target.value)
+	    }
     }
 
-    handleChange = (e) => {
-        this.setState({[e.target.name]: e.target.value})
-    }
-
-    handleFormSubmit = async (e) => {
+    const handleFormSubmit = async e => {
         e.preventDefault();
-        await axios.post("/api/signup", {username: this.state.username, password: this.state.password});
-        this.props.history.replace('/login');
+        await AuthAPI.signup({username: username, password: password});
+        history.replace('/login');
     }
 
-    componentDidMount() {
-        if(this.Auth.loggedIn()){
-            this.props.history.push('/dashboard')
-        }
-    }
-
-    render() {
-        return (
-            <Fragment>
-                <div className="main-wrapper">
-                    <div className="box">
-                        <div className="box-header">
-                            <h1>Signup</h1>
-                        </div>
-                        <form className="box-form">
-                            <input className="form-item" placeholder="Username" name="username" onChange={this.handleChange}/>
-                            <input className="form-item" placeholder="Password" name="password" type="password" onChange={this.handleChange}/>
-                            <button className="form-submit" onClick={this.handleFormSubmit}>Signup</button>
-                        </form>
-                        <Link className="link" to="/login">Already have an account? <span className="link-signup">Login</span></Link>
-                    </div>
-                </div>
-            </Fragment>
-        );
-    }
+    return (
+        <>
+            <form>
+                <h1>Signup</h1>
+                <input className="form-item" placeholder="Username" name="username" onChange={handleChange}/>
+                <input className="form-item" placeholder="Password" name="password" type="password" onChange={handleChange}/>
+                <button className="form-submit" onClick={handleFormSubmit}>Signup</button>
+                <Link className="link" to="/login">Already have an account? <span className="link-signup">Login</span></Link>
+            </form>
+        </>
+    );
 }
+
+export default Signup;
