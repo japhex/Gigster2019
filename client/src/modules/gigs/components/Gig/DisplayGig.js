4a569af0707117834af2ec28bab9b6661c5894dc
japhex <j@japhex.net>
2019-06-27 18:06:41 +0100

    Adding to extended gigs object, showing support acts #fuckingsmashingit

--- client/src/modules/gigs/components/Gig/DisplayGig.js
+++ client/src/modules/gigs/components/Gig/DisplayGig.js
@@ -6,6 +6,15 @@ const DisplayGig = ({gig, type, gigSource, withoutCrud, switchEditMode}) => {
 	return (
 		<>
 			<GigHeader gig={gig} type={type}/>
+			{/* Format into gigBody */}
+			{gig.supports &&
+				gig.supports.map((band, index) =>
+					<span>{band}{(index < 9 && index !== gig.supports.length - 1) && ', '}{index === 9 && '...'}</span>
+				)
+			}
+			{gig.festival &&
+				<>Festival</>
+			}
 			{!withoutCrud &&
 				<GigFooter gigId={gig.id} gigSource={gigSource} switchEditMode={switchEditMode} />
 			}
--- client/src/modules/middleware/utils.js
+++ client/src/modules/middleware/utils.js
@@ -1,10 +1,20 @@
-export const formatGig = (id, {performance, start, venue}) => {
+import React from "react"
+
+export const formatGig = (id, {performance, start, venue, type}) => {
 	const artist = performance.find(band => band.billingIndex === 1).displayName
+	const supportsArray = [];
+
+	performance.map((band, index) =>
+		(band.billingIndex !== 1 && index < 10) &&
+			supportsArray.push(band.displayName)
+	)
 
 	return {
 		id: id,
 		artist: artist,
 		date: start.date,
 		venue: venue.displayName,
+		supports: supportsArray,
+		festival: type.toLowerCase() === 'festival'
 	}
 }
